f61aabc66a18db2bfdcc8422e7a6cf909cfdee2f
Eden Mazzola <mazzolaeden@gmail.com>
2016-09-16 15:53:55 -0700

    add popup for twitter and facebook button

--- client/functions.js
+++ client/functions.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
+  $('.tell-friends').on('click', function(){
+    swal({
+      title: 'Where would you like to share?',
+      text: ['<a href="https://twitter.com/share" class="twitter-share-button"',
+      'data-show-count="false">TWEET</a><script async src="//platform.twitter.com/widgets.js"',
+      'charset="utf-8"></script>',
+      '<div id="shareBtn">FACEBOOK</div>'].join(''),
+      html: true,
+      confirmButtonColor: '#1CBC2C'
+    });
+    document.getElementById('shareBtn').onclick = function() {
+      FB.ui({
+        display: 'popup',
+        method: 'share',
+        href: 'https://stormy-caverns-60091.herokuapp.com/',
+      }, function(response){});
+    };
+  });
+
+
+
   $('.why-give i').on('click', function(){
     swal({
       title: "Because I need the cash, ok?!",
       confirmButtonColor: "#1CBC2C"
     });
   });
+
+
 
   $('.save-for-later').click(function(e) {
     e.preventDefault();
